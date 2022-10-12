4e41a147007dbf8b632b1ca7383438e160c60374
luismanti <luisenrique.mantillasarria@gmail.com>
2022-10-12 17:29:54 -0500

    coding

--- passwordGenerator/src/index.js
+++ passwordGenerator/src/index.js
@@ -29,3 +29,16 @@ const letters = [
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 const symbols = ["'", ":", "!", "@", "#", "$", "^", ")", "&", "*", "%", "-"];
+
+const getRandomNumber = (min, max) => {
+  return Math.floor(Math.random()* (max-min+1));
+}
+
+const arrayofArrays = [letters, numbers, symbols];
+let passwordLength = 10;
+
+
+for (const i=0; i< passwordLength; i++) {
+  const myArr = arrayofArrays[getRandomNumber(0, arrayofArrays.length-1)];
+}
+
